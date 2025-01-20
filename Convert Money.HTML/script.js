0d55df432b8c16b294cd3c65a8ce144c3658d225
predo-araujo <viniciuzaraujo@gmail.com>
2025-01-20 14:41:32 -0300

    1

--- Convert Money.HTML/script.js
+++ Convert Money.HTML/script.js
@@ -8,21 +8,23 @@ function ConvertValues() {
 
     const SelectCurrency = document.querySelector("Select.SelDolarEuro")
     
-    console.log(SelectCurrency.value)
     const dolarToday = 6.09
     const euroToday = 6.20
-
+   
+    
 
     if (SelectCurrency == "dolar") {
         OtherCurrency.innerHTML = new Intl.NumberFormat
-            ('en-US', { style: 'currency', currency: 'USD' }).format(InputValue / dolarToday)
+            ('en-US', { style: 'currency', currency: 'USD' }).format(InputValue/dolarToday)
     }
 
+    
     if (SelectCurrency == "euro") {
         OtherCurrency.innerHTML = new Intl.NumberFormat
             ('en-UK', { style: 'currency', currency: 'EUR' }).format(InputValue / euroToday)
 
     }
+    
 
     BRLValue.innerHTML = new Intl.NumberFormat
         ('pt-BR', { style: 'currency', currency: 'BRL' }).format(InputValue)
