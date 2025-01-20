7c1c083b23fa66232b90853e8d6941fa6fbdd1ba
predo-araujo <viniciuzaraujo@gmail.com>
2025-01-20 10:57:19 -0300

    Conversão dolar/euro

--- Convert Money.HTML/script.js
+++ Convert Money.HTML/script.js
@@ -1,35 +1,31 @@
-const ConvButton = document.querySelector ("button#convertButton")
+const ConvButton = document.querySelector("button#convertButton")
 
-function ConvertValues(){
-    const InputValue = document.querySelector ("input#inputConvert").value
+function ConvertValues() {
+    const InputValue = document.querySelector("input#inputConvert").value
 
-    const BRLValue = document.querySelector ("p#BRLvalue") // Valor Real
-    const OtherCurrency = document.querySelector ("p#OtherValue") // Outras Moedas
+    const BRLValue = document.querySelector("p#BRLvalue") // Valor Real
+    const OtherCurrency = document.querySelector("p#OtherValue") // Outras Moedas
 
-  
-    const dolarToday = 6.20
+    const SelectCurrency = document.querySelector("Select.SelDolarEuro")
+    
+    console.log(SelectCurrency.value)
+    const dolarToday = 6.09
+    const euroToday = 6.20
 
-    const FinalValue = dolarToday / InputValue
 
-    BRLValue.innerHTML = InputValue
+    if (SelectCurrency == "dolar") {
+        OtherCurrency.innerHTML = new Intl.NumberFormat
+            ('en-US', { style: 'currency', currency: 'USD' }).format(InputValue / dolarToday)
+    }
 
-    OtherCurrency.innerHTML = FinalValue
+    if (SelectCurrency == "euro") {
+        OtherCurrency.innerHTML = new Intl.NumberFormat
+            ('en-UK', { style: 'currency', currency: 'EUR' }).format(InputValue / euroToday)
 
+    }
 
     BRLValue.innerHTML = new Intl.NumberFormat
-    ('pt-BR', { style: 'currency', currency: 'BRL' }).format(InputValue)
-
-    OtherCurrency.innerHTML = new Intl.NumberFormat
-    ('en-US', { style: 'currency', currency: 'USD' }).format(FinalValue)
-
-   
-    
-
-    
-
-    
-
-    // console.log(FinalValue)
+        ('pt-BR', { style: 'currency', currency: 'BRL' }).format(InputValue)
 
 }
 
