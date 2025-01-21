ebe60e5a6d0e8b76675d645a2094bee43ee86070
predo-araujo <viniciuzaraujo@gmail.com>
2025-01-21 14:21:52 -0300

    Convertendo dolar e euro

--- Convert Money.HTML/script.js
+++ Convert Money.HTML/script.js
@@ -6,25 +6,25 @@ function ConvertValues() {
     const BRLValue = document.querySelector("p#BRLvalue") // Valor Real
     const OtherCurrency = document.querySelector("p#OtherValue") // Outras Moedas
 
-    const SelectCurrency = document.querySelector("Select.SelDolarEuro")
-    
+    const SelectCurrency = document.querySelector("Select.SelDolarEuro").value
+
+
     const dolarToday = 6.09
     const euroToday = 6.20
-   
-    
+
+
 
     if (SelectCurrency == "dolar") {
         OtherCurrency.innerHTML = new Intl.NumberFormat
-            ('en-US', { style: 'currency', currency: 'USD' }).format(InputValue/dolarToday)
+            ('en-US', { style: 'currency', currency: 'USD' }).format(InputValue / dolarToday)
     }
 
-    
+
     if (SelectCurrency == "euro") {
         OtherCurrency.innerHTML = new Intl.NumberFormat
             ('en-UK', { style: 'currency', currency: 'EUR' }).format(InputValue / euroToday)
 
     }
-    
 
     BRLValue.innerHTML = new Intl.NumberFormat
         ('pt-BR', { style: 'currency', currency: 'BRL' }).format(InputValue)
